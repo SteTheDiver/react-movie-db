dcc96412f9609c1ea62165b7575421dadaf6622d
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-03-29 22:28:26 +0200

    Creation of home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import Filters from "../components/Filters/Filters";
+import MoviesList from "../components/MoviesList/MoviesList";
+import SearchBar from "../components/SearchBar/SearchBar";
+
+function Home() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        margin: "0 auto",
+        width: "70%",
+      }}
+    >
+      <SearchBar />
+      <Filters />
+      <MoviesList />
+    </div>
+  );
+}
+
+export default Home;
