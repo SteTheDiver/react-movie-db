ce9cb90aacd1bbc5ba7d3e05b78847c5cf5c91e5
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-04-10 17:40:53 +0200

    Modified styling

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styles from "./Error.module.scss";
 
 function Error() {
   return (
     //add a funny 404 image
-    <section>
-      <div>404</div>
-      <h1>Page not found</h1>
-
-      <Link to="/">Back to Home</Link>
-    </section>
+    <main className={styles.Error}>
+      <span className={styles.Text}>Error 404</span>
+      <span className={styles.Text}>Page not found</span>
+      <Link to="/">
+        <button className={styles.Button}>Back to Home</button>
+      </Link>
+    </main>
   );
 }
 
