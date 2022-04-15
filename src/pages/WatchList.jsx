c5c9fe4bf3f193eb15793dde9767e41023232e06
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-04-15 16:10:37 +0200

    basic inline styling and added favourites component

--- src/pages/WatchList.jsx
+++ src/pages/WatchList.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import Favourites from "../components/Favourites/Favourites";
+
+import { useGlobalContext } from "../context";
 
 function WatchList() {
+  const { favourites } = useGlobalContext();
+
+  console.log(favourites, "fav");
   return (
-    <div>WatchList</div>
-  )
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        margin: "0 auto",
+        backgroundColor: "#071423",
+        height: "100vh"
+      }}
+    >
+      <span>My Watch List</span>
+      {favourites.length === 0 ? <span>No movies </span> : <Favourites />}
+    </div>
+  );
 }
 
-export default WatchList
+export default WatchList;
