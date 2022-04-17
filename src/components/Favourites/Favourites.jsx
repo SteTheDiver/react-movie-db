2327b4ea0e016c4cd674cd58dda9db385f332138
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-04-17 18:41:02 +0200

    Modified  favourite styling for footer

--- src/components/Favourites/Favourites.jsx
+++ src/components/Favourites/Favourites.jsx
@@ -1,4 +1,6 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { Link } from "react-router-dom";
+
 import styles from "./Favourites.module.scss";
 
 import CancelIcon from "@mui/icons-material/Cancel";
@@ -8,32 +10,40 @@ import { useGlobalContext } from "../../context";
 function Favourites() {
   const { favourites, removeFromList, clearList } = useGlobalContext();
 
-  // useEffect(() => {}, [favourites]);
-
   const imageUrl = "https://image.tmdb.org/t/p/w500";
 
   return (
     <main className={styles.Favourites}>
-      {favourites.map((favourite) => {
-        return (
-          <article className={styles.Container} key={favourite.id}>
-            <img
-              src={imageUrl + favourite.backdrop_path}
-              alt={favourite.title}
-            />
-            <span>{favourite.title}</span>
-            <CancelIcon
-              onClick={() => removeFromList(favourite)}
-              className={styles.Icon}
-            />
-          </article>
-        );
-      })}
-      {favourites.length > 0 ? (
-        <button className={styles.Button} onClick={() => clearList()}>
-          clear list
-        </button>
-      ) : null}
+      <div className={styles.Grid}>
+        {favourites.map((favourite) => {
+          console.log(favourite);
+          return (
+            <article className={styles.Container} key={favourite.id}>
+              <Link to={`movies/${favourite.id}`}>
+                <img
+                  src={imageUrl + favourite.backdrop_path}
+                  alt={favourite.title}
+                />
+              </Link>
+              <footer className={styles.Footer}>
+                <section className={styles.Info}>
+                  <span>{favourite.title}</span>
+                  <span> Released: {favourite.release_date}</span>
+                </section>
+                <CancelIcon
+                  onClick={() => removeFromList(favourite)}
+                  className={styles.Icon}
+                />
+              </footer>
+            </article>
+          );
+        })}
+      </div>
+        {favourites.length > 0 ? (
+          <button className={styles.Button} onClick={() => clearList()}>
+            clear list
+          </button>
+        ) : null}
     </main>
   );
 }
