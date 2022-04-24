128795728bd32d54fc5951fd6def1049d2db9733
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-04-24 18:36:27 +0200

    Imported Footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 
 import { BrowserRouter, Routes, Route, Link, Outlet } from "react-router-dom";
 import Error from "./components/Error/Error";
-import Navbar from "./components/Navbar/Navbar";
+import Footer from "./Layouts/Footer/Footer";
+import Navbar from "./Layouts/Navbar/Navbar";
 import SingleMovie from "./components/SingleMovie/SingleMovie";
 import Home from "./pages/Home";
 import Upcoming from "./pages/Upcoming";
@@ -21,6 +22,7 @@ function App() {
         <Route path="/watchList" element={<WatchList/>} />
         <Route path="*" element={<Error />} />
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }
