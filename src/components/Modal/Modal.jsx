57bc533e522cb5e523714a8337404ca5558e7d25
SteTheDiver <stefano.cattaneo86@gmail.com>
2022-05-05 16:03:26 +0200

    Added useEffect logic

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import styles from "./Modal.module.scss";
 
+import { useGlobalContext } from "../../context";
+
 function Modal(props) {
+  const { showModal, setShowModal } = useGlobalContext();
+
+  
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowModal(!showModal);
+    }, 4000);
+    return () => clearTimeout(timer);
+  }, [setShowModal, showModal]);
+
+
   return (
     <div className={styles.Modal}>
       <span className={styles.Text}>{props.message}</span>
